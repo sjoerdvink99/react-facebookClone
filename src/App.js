95b387857152d99bd2860816f3d5ab6139df7986
Sjoerd Vink <sjoerdvink@icloud.com>
2021-02-23 10:44:27 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+const tijd = "0.08.00";
+
+function App() {
+  return (
+    <div className='app'>
+      <h1>Facebook</h1>
+    </div>
+  );
+}
+
+export default App;
